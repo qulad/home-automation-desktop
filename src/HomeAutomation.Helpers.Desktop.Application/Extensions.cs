46a4321e51ef1a87f3eabce3733544f55e2295e0
qulad <ahmetertugrulkaya@gmail.com>
2023-12-19 22:46:55 +0300

    use Feature sender

--- src/HomeAutomation.Helpers.Desktop.Application/Extensions.cs
+++ src/HomeAutomation.Helpers.Desktop.Application/Extensions.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using HomeAutomation.Helpers.Desktop.Features;
 
 namespace HomeAutomation.Helpers.Desktop.Application;
 
@@ -7,7 +8,23 @@ public static class Extensions
 {
     public static IServiceCollection UseApplication(this IServiceCollection services)
     {
+        services
+            .UseHostedService()
+            .UseFeatureSender();
+
+        return services;
+    }
+
+    public static IServiceCollection UseHostedService(this IServiceCollection services)
+    {
         services.AddHostedService<HostedService>();
+
+        return services;
+    }
+
+    public static IServiceCollection UseFeatureSender(this IServiceCollection services)
+    {
+        services.AddSingleton<IFeatureSender, FeatureSender>();
 
         return services;
     }
