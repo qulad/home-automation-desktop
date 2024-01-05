a38c1a0a93635b5a1218e6c467b92c322c69c633
qulad <ahmetertugrulkaya@gmail.com>
2024-01-06 00:15:39 +0300

    remove nullable

--- src/HomeAutomation.Helpers.Desktop.Application/Services/Clients/ICommadLineServiceClient.cs
+++ src/HomeAutomation.Helpers.Desktop.Application/Services/Clients/ICommadLineServiceClient.cs
@@ -1,9 +1,8 @@
 using System.Collections.Generic;
-using System.Diagnostics;
 
 namespace HomeAutomation.Helpers.Desktop.Application.Services.Clients;
 
 public interface  ICommandLineServiceClient
 {
-    public string ExecuteCommand(string command, IList<string>? args = null, IDictionary<string, string>? kwargs = null);
+    public string ExecuteCommand(string command, IList<string> args = null, IDictionary<string, string> kwargs = null);
 }
