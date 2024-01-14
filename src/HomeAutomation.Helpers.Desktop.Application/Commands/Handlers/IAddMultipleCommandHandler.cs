83d954fc96b9fa10e38e2df8852634d499595ca6
qulad <ahmetertugrulkaya@gmail.com>
2024-01-15 02:46:01 +0300

    add multiple command handler interface

--- src/HomeAutomation.Helpers.Desktop.Application/Commands/Handlers/IAddMultipleCommandHandler.cs
+++ src/HomeAutomation.Helpers.Desktop.Application/Commands/Handlers/IAddMultipleCommandHandler.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace HomeAutomation.Helpers.Desktop.Application.Commands.Handlers;
 
-public interface IAddMultipleCommandHandler<TCommand> where TCommand : ICommand
+public interface IAddMultipleCommandHandler<in TCommand> where TCommand : ICommand
 {
-    void Handle(IList<TCommand> commands);
+    void Handle(TCommand command);
 }
