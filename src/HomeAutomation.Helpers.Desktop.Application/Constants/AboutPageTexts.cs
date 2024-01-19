930d03aa75b403e66a21e5a9fb3cd2205cac1b51
qulad <ahmetertugrulkaya@gmail.com>
2024-01-19 18:49:43 +0300

    remove unused

--- src/HomeAutomation.Helpers.Desktop.Application/Constants/AboutPageTexts.cs
+++ src/HomeAutomation.Helpers.Desktop.Application/Constants/AboutPageTexts.cs
@@ -3,5 +3,4 @@ namespace HomeAutomation.Helpers.Desktop.Application.Constants;
 public static class AboutPageTexts
 {
     public const string AboutText = "Bu uygulama bitirme projemizin bir parçaşıdır.\nAhmet Ertuğrul KAYA ve Eren AY";
-    public const string HomePageButton = "Ana sayfa";
 }
