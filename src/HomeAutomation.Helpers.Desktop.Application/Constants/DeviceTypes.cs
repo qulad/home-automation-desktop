76bc46e2067532ed4f4ec06b6f8af1cad4b0ffb5
qulad <ahmetertugrulkaya@gmail.com>
2024-01-23 00:04:57 +0300

    DeviceTypes constant

---
+++ src/HomeAutomation.Helpers.Desktop.Application/Constants/DeviceTypes.cs
@@ -0,0 +1,7 @@
+namespace HomeAutomation.Helpers.Desktop.Application.Constants;
+
+public static class DeviceTypes
+{
+    public const string Analog = nameof(Analog);
+    public const string Digital = nameof(Digital);
+}
