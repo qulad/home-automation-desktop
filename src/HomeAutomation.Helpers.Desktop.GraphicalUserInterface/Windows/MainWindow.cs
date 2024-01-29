dbce98843742ba0107096c6668502254cb2345c0
qulad <ahmetertugrulkaya@gmail.com>
2024-01-29 22:16:31 +0300

    remove sonarlint wanings

--- src/HomeAutomation.Helpers.Desktop.GraphicalUserInterface/Windows/MainWindow.cs
+++ src/HomeAutomation.Helpers.Desktop.GraphicalUserInterface/Windows/MainWindow.cs
@@ -121,30 +121,20 @@ public partial class MainWindow : Form
 
     private void AddConnectionPageButtonClick(object sender, EventArgs e)
     {
-        ActivateButton(sender, RGBColors.color1);
+        ActivateButton(sender, Color.FromArgb(172, 126, 241));
         OpenChildUserControl(_basePage.Get<AddConnectionPage>());
     }
 
     private void SelectConnectionsButtonClick(object sender, EventArgs e)
     {
-        ActivateButton(sender, RGBColors.color2);
+        ActivateButton(sender, Color.FromArgb(249, 118, 176));
         OpenChildUserControl(_basePage.Get<SelectConnectionPage>());
     }
 
     private void AboutPageButtonClick(object sender, EventArgs e)
     {
-        ActivateButton(sender, RGBColors.color3);
+        ActivateButton(sender, Color.FromArgb(253, 138, 114));
         OpenChildUserControl(_basePage.Get<AboutPage>());
-    }
-
-    private struct RGBColors
-    {
-        public static Color color1 = Color.FromArgb(172, 126, 241);
-        public static Color color2 = Color.FromArgb(249, 118, 176);
-        public static Color color3 = Color.FromArgb(253, 138, 114);
-        public static Color color4 = Color.FromArgb(95, 77, 221);
-        public static Color color5 = Color.FromArgb(249, 88, 155);
-        public static Color color6 = Color.FromArgb(24, 161, 251);
     }
 
     private void ActivateButton(object senderBtn, Color color)
